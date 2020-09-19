7c183bff657b9a04e79dae492e18473a36e07edd
Miłosz Cuppa <Cuppek@users.noreply.github.com>
2020-09-19 20:43:59 +0200

    Create hello5.py

---
+++ hello5.py
@@ -0,0 +1,11 @@
+from sklearn.model_selection import KFold
+import pandas as pd
+
+df = pd.read_csv('https://sololearn.com/uploads/files/titanic.csv')
+X = df[['Age', 'Fare']].values[:6]
+y = df['Survived'].values[:6]
+
+kf = KFold(n_splits=3, shuffle=True)
+
+for train, test in kf.split(X):
+    print(train, test)
