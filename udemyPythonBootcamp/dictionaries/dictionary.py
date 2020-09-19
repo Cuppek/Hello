ada2fc2e016914bec867920fc7a3c2449f1bcb5d
Cuppek <Cuppek@users.noreply.github.com>
2020-09-19 20:43:19 +0200

    face recognition

--- udemyPythonBootcamp/dictionaries/dictionary.py
+++ udemyPythonBootcamp/dictionaries/dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 data = json.load(open(r"C:\Users\milos\Desktop\Hello\udemyPythonBootcamp\dictionaries\data.json"))
 
 def translate(word):
@@ -9,8 +10,15 @@ def translate(word):
         return data[word.title()]
     elif word.upper() in data:
         return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        print("miałeś na myśli %s?" %get_close_matches(word, data()))
+        decide = input("\nWciśnij t jeżeli tak lub n jeżeli nie")
+        if decide == "y":
+            return data[get_close_matches(word, data())]
+        elif decide == "n":
+            return("Nie ma takiego słowa")
     else:
-        print("Nie ma takiego słowa")
+        print("Nie ma takiego słowa2")
 
 word = input("Wpisz słowo: ")
 output = translate(word)
