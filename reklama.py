f3bc91db0ee127b7ee15d6ac4435ca3b2694cba6
Miłosz Cuppa <Cuppek@users.noreply.github.com>
2020-10-01 00:30:57 +0200

    Update reklama.py

--- reklama.py
+++ reklama.py
@@ -1,10 +1,13 @@
 time = int(input('Wpisz długość filmu w minutach: '))
 i = time
-reklam = 0
+reklam = -1
 
+if time > 40:
+    while time >= 40:
+        reklam +=1
+        time -= 40
+else:
+    reklam = 0
 
-while time > 40:
-    reklam +=1
-    time -= 40
 
 print(f"Podczas filmu zostanie wyświetlone {reklam} reklam, łączny czas antenowy wynosi {i + reklam * 20} minut.")
